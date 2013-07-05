8c7ee7428423d5271fc26a6ffd581759d269d986
Limaa <guilima10@gmail.com>
2013-07-04 23:54:31 -0300

    Commiting the rest of the exercises

---
+++ ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // puts()
+#include <stdlib.h> // system(char*) executa um comando shell
+
+int main(int argc, char *argv[])
+{
+	system("clear");
+	puts("Hello world.");
+	return 0;
+}
